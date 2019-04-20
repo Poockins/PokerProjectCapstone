1bcac0844fcec29a02cb8277bf9999f59e6b5cde
p0pepalpatine <46880643+p0pepalpatine@users.noreply.github.com>
2019-04-20 10:15:56 -0600

    Redid the GUIs
    Kept the GUI forms and added the new screens. Currently the number of players doesn't affect anything nor do the clear form buttons work. The working buttons are ones that change the screens from the history to the calculator. The table that would show probability doesn't display anything, but I did have it set up to take in a string and display that string.

---
+++ src/PokerProjectCapstone/src/main/java/calculator/DBConnection.java
@@ -0,0 +1,97 @@
+/**
+ * DBConnection is used for database functionality throughout the app
+ *
+ * @author Yuko Takegoshi
+ * @version 1.0
+ */
+
+package calculator;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnection {
+
+  static final String DB_PATH = "data/poker";
+  public Connection conn; // Persist database connection until explicitly shut down
+
+  /**
+   * Constructor
+   * @throws Exception
+   */
+  public DBConnection() throws Exception {
+    Class.forName("org.hsqldb.jdbcDriver");
+
+    conn = DriverManager.getConnection("jdbc:hsqldb:file:" + DB_PATH, "SA", "");
+  }
+
+  /**
+   * Initializes a database and table structure if they do not yet exist
+   *
+   * @return Nothing
+   */
+  public void setup() throws SQLException {
+    createGamesTable();
+    createPlayersTable();
+    createHandsTable();
+  }
+
+  /**
+   * Initializes the games table if it doesn't already exist
+   *
+   * @return Nothing
+   * @throws SQLException on creation failure
+   */
+  private void createGamesTable() throws SQLException {
+    String query = "CREATE TABLE IF NOT EXISTS games (" +
+        "id INT IDENTITY PRIMARY KEY, " +
+        "flop VARCHAR(30) ARRAY, " +
+        "turn VARCHAR(30), " +
+        "river VARCHAR(30), " +
+        "created_at TIMESTAMP);";
+
+    Statement st = conn.createStatement();
+    st.executeUpdate(query);
+    st.close();
+  }
+
+  /**
+   * Initializes the players table if it doesn't already exist
+   *
+   * @return Nothing
+   * @throwsSQLException on creation failure
+   */
+  private void createPlayersTable() throws SQLException {
+    String query = "CREATE TABLE IF NOT EXISTS players (" +
+        "id INT IDENTITY PRIMARY KEY, " +
+        "name VARCHAR(256));";
+
+    Statement st = conn.createStatement();
+    st.executeUpdate(query);
+    st.close();
+  }
+
+  /**
+   * Initializes the hands table if it doesn't already exist
+   *
+   * @return Nothing
+   * @throws SQLException on creation failure
+   */
+  private void createHandsTable() throws SQLException {
+    String query = "CREATE TABLE IF NOT EXISTS hands (" +
+        "id INT IDENTITY PRIMARY KEY, " +
+        "cards VARCHAR(30) ARRAY, " +
+        "player_id INT, " +
+        "game_id INT, " +
+        "FOREIGN KEY(player_id) REFERENCES players, " +
+        "FOREIGN KEY(game_id) REFERENCES games);";
+
+    Statement st = conn.createStatement();
+    st.executeUpdate(query);
+    st.close();
+  }
+}
---
+++ src/PokerProjectCapstone/src/test/java/calculator/DBConnectionTests.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class DBConnectionTests {
+  static DBConnection db;
+
+  @BeforeEach void initDBConnection() {
+    try {
+      db = new DBConnection();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @AfterEach void closeDBConnection() {
+    try {
+      db.conn.close();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  @DisplayName("DB setup")
+  void setupTest() {
+    try {
+      db.setup();
+      Statement statement = db.conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+      ResultSet result = statement.executeQuery("SELECT COUNT(TABLE_NAME) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'");
+      result.first();
+      assertEquals(3, result.getInt(1));
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
