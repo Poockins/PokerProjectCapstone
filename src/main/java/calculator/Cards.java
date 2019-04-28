ff16d4f2ed7da50fae78e8f59ad18c604989fc28
Yuko Takegoshi <vlyandra@gmail.com>
2019-04-28 15:38:17 -0400

    Database integration for Players -- Changes to StartCalculator to always set up the database on app start

--- src/main/java/calculator/Cards.java
+++ src/main/java/calculator/Cards.java
@@ -9,56 +9,65 @@ import java.util.Map;
 
 public class Cards implements Comparable<Cards> {
 
-    private final Rank rank;
-    private final Suit suit;
+  private final Rank rank;
+  private final Suit suit;
 
-    private final static Map<String, Cards> CARD_CONTAINER = initContainer();
+  public Cards(final Rank rank, final Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
 
-    private static Map<String, Cards> initContainer() {
-        final Map<String, Cards> container = new HashMap<>();
-        for (final Suit suit : Suit.values()) {
-            for (final Rank rank : Rank.values()) {
-                container.put(cardName(rank, suit), new Cards(rank, suit));
-            }
-        }
-        return Collections.unmodifiableMap(container);
+
+  private final static Map<String, Cards> CARD_CONTAINER = initContainer();
+
+  private static Map<String, Cards> initContainer() {
+    final Map<String, Cards> container = new HashMap<>();
+    for (final Suit suit : Suit.values()) {
+      for (final Rank rank : Rank.values()) {
+        container.put(cardName(rank, suit), new Cards(rank, suit));
+      }
     }
+    return Collections.unmodifiableMap(container);
+  }
 
-    static Cards getCard(final Rank rank, final Suit suit) {
-        final Cards card = CARD_CONTAINER.get(cardName(rank, suit));
-        if (card != null) {
-            return card;
-        }
-        throw new RuntimeException("NOT a card ! " + rank + " " + suit);
+  static Cards getCard(final Rank rank, final Suit suit) {
+    final Cards card = CARD_CONTAINER.get(cardName(rank, suit));
+    if (card != null) {
+      return card;
     }
+    throw new RuntimeException("NOT a card ! " + rank + " " + suit);
+  }
 
-    public Rank getRank() {
-        return this.rank;
-    }
+  public Rank getRank() {
+    return this.rank;
+  }
 
-    public Suit getSuit() {
-        return this.suit;
-    }
+  public Suit getSuit() {
+    return this.suit;
+  }
 
-    private static String cardName(final Rank rank,final Suit suit) {
-        return rank + " of " + suit;
-    }
+  private static String cardName(final Rank rank, final Suit suit) {
+    return rank + " of " + suit;
+  }
 
-    public Cards(final Rank rank, final Suit suit) {
-        this.rank = rank;
-        this.suit = suit;
-    }
+  /**
+   * Used for storing cards in the database as strings
+   *
+   * @return database friendly string representation of the card
+   */
+  public String toDataString() {
+    return String.format("%s::%s", this.rank, this.suit);
+  }
 
-    @Override
-    public String toString() {
-        return String.format("%s of %s", this.rank, this.suit);
-    }
+  @Override
+  public String toString() {
+    return String.format("%s of %s", this.rank, this.suit);
+  }
 
-    @Override
-    public int compareTo(final Cards val) {
-        final int rankCompar = Integer.compare(this.rank.getRankValue(), val.rank.getRankValue());
-        return rankCompar != 0 ? rankCompar : Integer.compare(this.suit.getSuitValue(), val.suit.getSuitValue());
-    }
-
-
+  @Override
+  public int compareTo(final Cards val) {
+    final int rankCompar = Integer.compare(this.rank.getRankValue(), val.rank.getRankValue());
+    return rankCompar != 0 ? rankCompar
+        : Integer.compare(this.suit.getSuitValue(), val.suit.getSuitValue());
+  }
 }
--- src/main/java/calculator/Player.java
+++ src/main/java/calculator/Player.java
@@ -1,42 +1,107 @@
-/*
- * 
+/**
+ * Represents a player
+ *
  * @author rob
+ * @author Yuko Takegoshi
  * @version 1.0
  */
 package calculator;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class Player {
 
-    private String name;
-    private ArrayList<Cards> cards = new ArrayList<>();
+  private String name;
+  private int id;
 
-    public String getName() {
-        return name;
+  /**
+   * Constructor
+   *
+   * @param name player name
+   */
+  public Player(String name) {
+    try {
+      DBConnection db = new DBConnection();
+      String query = String.format("INSERT INTO players (name) VALUES (%s)", name);
+      ArrayList<Integer> keys = db.insertQuery(query);
+      this.name = name;
+      this.id = keys.get(0);
+      db.conn.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  /**
+   * Constructor given database id and name
+   * Assumes that the record already exists as given in the database
+   *
+   * @param id Primary key database id of player
+   * @param name Player name
+   */
+  public Player(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  /**
+   * Gets the player name
+   *
+   * @return player name
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Updates the player name
+   *
+   * @param name Name of the player
+   * @return Nothing
+   */
+  public void setName(String name) {
+    try {
+      DBConnection db = new DBConnection();
+      String query = String.format("UPDATE players SET name = %s WHERE id = %i", name, this.id);
+      db.updateQuery(query);
+      this.name = name;
+      db.conn.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  /**
+   * Gets the Primary Key id of the Player
+   *
+   * @return id of the Player
+   */
+  public int getId() {
+    return this.id;
+  }
+
+  /**
+   * Finds a Player from the database given an id
+   *
+   * @return Player from found player data
+   */
+  public Player findById(int id) {
+    String foundName = null;
+    try {
+      DBConnection db = new DBConnection();
+      ResultSet found = db.selectQuery("SELECT * FROM players WHERE id=" + id);
+      found.first();
+      foundName = found.getString(2);
+      db.conn.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
 
-    public void setName(String name) {
-        this.name = name;
+    if (foundName != null) {
+      return new Player(id, foundName);
+    } else {
+      return null;
     }
-
-    public ArrayList<Cards> getCards() {
-        return this.cards;
-    }
-
-    public void setCards(ArrayList<Cards> cards) {
-        this.cards = cards;
-    }
-
-    Player(String name) {
-        this.name = name;
-    }
-
-    public void addCard(Stack<Cards> s) {
-
-        cards.add(s.pop());
-
-    }
-
+  }
 }
