db54c61fa72953b1244507ed47524894d34f6e6c
Robeee <49735982+Robeee@users.noreply.github.com>
2019-05-12 10:31:15 -0400

    Add files via upload
    More descriptive log contents.

--- src/main/java/StartCalculator.java
+++ src/main/java/StartCalculator.java
@@ -1,42 +1,44 @@
-
 /**
  * Main/Driver class for poker calculator
  *
  * @author Yuko Takegoshi
  */
-import calculator.*;
+
+import calculator.DBConnection;
+import calculator.WelcomeScreen;
 import java.io.IOException;
 import java.sql.SQLException;
-import javax.swing.JFrame;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.*;
 
 public class StartCalculator {
-
+    
     private final static Logger STARTLOGGER = Logger.getLogger("Start Calc");
     private static Handler fileHandler;
 
-    public static void main(String[] args) {
-        try {
+  public static void main(String[] args) {
+      try {
             fileHandler = new FileHandler("startLogger.log", true);
             STARTLOGGER.addHandler(fileHandler);
         } catch (IOException exception) {
             STARTLOGGER.log(Level.FINER, "FileHandler error.", exception);
         }
-        try {
-            // Database setup on app start
-            DBConnection db = new DBConnection();
-            db.setup();
-            // End database setup
-        } catch (SQLException ex) {
-            STARTLOGGER.log(Level.FINE, "DB error.", ex);
-        }
-
-        WelcomeScreen newGame = new WelcomeScreen();
-        newGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        newGame.setLocation(300, 300);
-        newGame.setVisible(true);
+    try {
+      // Database setup on app start
+      DBConnection db = new DBConnection();
+      db.setup();
+      // End database setup
+    } catch (SQLException ex) {
+      STARTLOGGER.log(Level.FINE, "DB setup error.", ex);
     }
+
+    WelcomeScreen newGame = new WelcomeScreen();
+    newGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    newGame.setLocation(300, 300);
+    newGame.setVisible(true);
+  }
 }
